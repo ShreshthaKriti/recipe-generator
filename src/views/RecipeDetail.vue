409885ce696fc40095ee5132e5a2ce870eeb7f35
ShreshthaKriti <kritishreshtha08@gmail.com>
2023-11-27 15:26:15 +0100

    added responsiveness

--- src/views/RecipeDetail.vue
+++ src/views/RecipeDetail.vue
@@ -1,71 +1,68 @@
 <template>
-  <v-container v-if="recipeDetail && recipeDetail.media && recipeDetail.media.images['ratio_16:9']" fluid>
+  <v-container fluid v-if="recipeDetail && recipeDetail.media && recipeDetail.media.images['ratio_16:9']">
     <v-row>
-      <v-col cols="8">
-        <div>
-          <v-img
-            :src="recipeDetail.media.images['ratio_16:9'].url"
-            :alt="recipeDetail.media.images['ratio_16:9'].altTag"
-          ></v-img>
+      <v-col cols="12" md="8">
+        <v-img
+          :src="recipeDetail.media.images['ratio_16:9'].url"
+          :alt="recipeDetail.media.images['ratio_16:9'].altTag"
+          class="mb-4"
+        ></v-img>
+
+        <div class="steps">
+          <h1>{{ recipeDetail.title }}</h1>
+          <p><i>{{ recipeDetail.media.images['ratio_16:9'].caption }}</i></p>
+          <br/>
+          <div v-if="recipeDetail.labels">
+            <v-row>
+              <v-col cols="auto" v-for="label in recipeDetail.labels" :key="label">
+                <v-chip>{{ label }}</v-chip>
+              </v-col>
+            </v-row>
+          </div>
+          <br/>
+          <h3>Steps</h3>
+          <div v-if="recipeDetail.recipeStepGroups[0].recipeSteps">
+            <ul>
+              <li v-for="(step, index) in sortedSteps" :key="step.recipeStepOrder">
+                {{ step.instruction }}
+              </li>
+            </ul>
+          </div>
         </div>
       </v-col>
-      <v-col cols="4" class="ingredients">
-        <h3>Ingredient list</h3>
-        <div v-if="recipeDetail.ingredientGroups">
-          <ul v-for="ingredientList in recipeDetail.ingredientGroups">
-            <li>
-              <h4>{{ ingredientList.name }}</h4>
-              <div v-for="ingredient in ingredientList.ingredientGroupIngredients">
-                <li>
-                  {{ ingredient.quantity }} {{ ingredient.unit }} {{ ingredient.ingredient }}
-                </li>
-              </div>
-            </li>
-            <br/>
-          </ul>
+
+      <v-col cols="12" md="4">
+        <div class="ingredients mb-4">
+          <h3>Ingredient list</h3>
+          <div v-if="recipeDetail.ingredientGroups">
+            <ul v-for="ingredientList in recipeDetail.ingredientGroups" :key="ingredientList.name">
+              <li>
+                <h4>{{ ingredientList.name }}</h4>
+                <ul>
+                  <li v-for="ingredient in ingredientList.ingredientGroupIngredients" :key="ingredient.ingredient">
+                    {{ ingredient.quantity }} {{ ingredient.unit }} {{ ingredient.ingredient }}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <v-btn>Add missing ingredients</v-btn>
         </div>
-        <VBtn>
-          Add missing ingredients
-        </VBtn>
+
+        <div class="rating">
+          <h3>Recipe Info</h3>
+          <div>
+            <v-icon>mdi-clock-time-five-outline</v-icon> Cooking time: {{ recipeDetail.preparationTime }} minutes
+          </div>
+          <div>
+            <v-icon>mdi-chef-hat</v-icon> Difficulty: {{ recipeDetail.difficulty }}
+          </div>
+          <div>
+            <v-icon>mdi-star</v-icon> Rating: {{ recipeDetail.rating.stars }}
+          </div>
+        </div>
       </v-col>
     </v-row>
-    <v-row>
-    <v-col cols="8" class="steps">
-      <h1>{{ recipeDetail.title }}</h1>
-      <p><i>{{ recipeDetail.media.images['ratio_16:9'].caption }}</i></p>
-      <br/>
-      <div v-if="recipeDetail.labels">
-        <v-row> <!-- Use v-row here for horizontal layout -->
-          <v-col cols="auto" v-for="label in recipeDetail.labels" :key="label">
-            <v-chip>
-              {{ label }}
-            </v-chip>
-          </v-col>
-        </v-row>
-      </div>
-      <br/>
-      <h3>Steps</h3>
-      <div v-if="recipeDetail.recipeStepGroups[0].recipeSteps">
-        <ul>
-          <li v-for="(step, index) in sortedSteps" :key="step.recipeStepOrder">
-            {{ step.instruction }}
-          </li>
-        </ul>
-      </div>
-    </v-col>
-    <v-col cols="4" class="rating">
-      <h3>Recipe Info</h3>
-      <div>
-        <v-icon>mdi-clock-time-five-outline</v-icon> Cooking time: {{ recipeDetail.preparationTime }} minutes
-      </div>
-      <div>
-        <v-icon>mdi-chef-hat</v-icon> Difficulty: {{ recipeDetail.difficulty }}
-      </div>
-      <div>
-        <v-icon>mdi-star</v-icon> Rating: {{ recipeDetail.rating.stars }}
-      </div>
-    </v-col>
-  </v-row>
   </v-container>
 </template>
  
